0be2fe4da1ceaedd4ab0794a1fe9195c92153b73
ThunderMiracle <cissoliu@gmail.com>
2022-07-29 12:04:24 +0900

    first commit

---
+++ pages/api/form.ts
@@ -0,0 +1,16 @@
+import { NextResponse } from 'next/server';
+
+import type { NextRequest } from 'next/server';
+
+export default function form(req: NextRequest) {
+  console.log(req);
+  return NextResponse.json({
+    domain: 'joinsure-dev-portal.jp.auth0.com',
+    clientId: 'LTGqM8sO4K6ZUzsViFTVeHNRn4ecCZBa',
+    connectionName: 'justincase-dev',
+  });
+}
+
+export const config = {
+  runtime: 'experimental-edge',
+};
