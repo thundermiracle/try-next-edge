e7a2ab4f4bea2d4faeb751f748b1053a3b6a1c89
ThunderMiracle <cissoliu@gmail.com>
2022-07-29 12:11:22 +0900

    chore: try print out req.url

--- pages/api/form.ts
+++ pages/api/form.ts
@@ -1,16 +1,17 @@
-import { NextResponse } from 'next/server';
+import { NextResponse } from "next/server";
 
-import type { NextRequest } from 'next/server';
+import type { NextRequest } from "next/server";
 
 export default function form(req: NextRequest) {
-  console.log(req);
+  console.log("req.url:", req.url);
+  console.log("req.query:", req.referrer);
   return NextResponse.json({
-    domain: 'joinsure-dev-portal.jp.auth0.com',
-    clientId: 'LTGqM8sO4K6ZUzsViFTVeHNRn4ecCZBa',
-    connectionName: 'justincase-dev',
+    domain: "joinsure-dev-portal.jp.auth0.com",
+    clientId: "LTGqM8sO4K6ZUzsViFTVeHNRn4ecCZBa",
+    connectionName: "justincase-dev",
   });
 }
 
 export const config = {
-  runtime: 'experimental-edge',
+  runtime: "experimental-edge",
 };
