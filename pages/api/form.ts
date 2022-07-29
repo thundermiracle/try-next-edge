f7536df39f9fd5d7e4bd811d22f7b7fa4a8c0935
ThunderMiracle <cissoliu@gmail.com>
2022-07-29 13:01:39 +0900

    chore: try print sourcePage

--- pages/api/form.ts
+++ pages/api/form.ts
@@ -2,8 +2,9 @@ import { NextResponse } from "next/server";
 
 import type { NextRequest } from "next/server";
 
-export default function form(req: NextRequest) {
+export default function form(req: any) {
   console.log("req.url:", req.url);
+  console.log("req.sourcePage:", req.sourcePage);
   console.log("req:", JSON.stringify(req));
   return NextResponse.json({
     domain: "joinsure-dev-portal.jp.auth0.com",
